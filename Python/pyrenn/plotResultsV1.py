b46aba1a2abd6c39cf21554f8d31ec4a0b0ee3ab
arnoplaetinck <arno.plaetinck@hotmail.com>
2020-03-17 14:26:58 +0100

    MP 17/03

---
+++ Python/pyrenn/plotResultsV1.py
@@ -0,0 +1,212 @@
+import csv
+from statistics import mean
+import matplotlib.pyplot as plt
+import numpy as np
+
+iterations = 20
+
+name_train_PC = "MP_NN_ALL_TRAIN_PC_Tue_Mar_17_13_06_07_2020"
+name_run_PC = "MP_NN_ALL_RUN_PC_Tue_Mar_17_13_07_53_2020"
+
+name_train_PI = "MP_NN_ALL_TRAIN_PI3B_Mon_Mar_16_15_55_25_2020"
+name_run_PI = "MP_NN_ALL_RUN_PI3B_Mon_Mar_16_16_06_44_2020"
+
+name_train_NANO = "MP_NN_ALL_TRAIN_NANO_Mon_Mar_16_12_28_53_2020"
+name_run_NANO = "MP_NN_ALL_RUN_NANO_Mon_Mar_16_12_47_21_2020"
+
+name_train_CORAL = "MP_NN_ALL_TRAIN_CORAL_Mon_Mar_16_13_30_00_2020"
+name_run_CORAL = "MP_NN_ALL_RUN_CORAL_Mon_Mar_9_13_20_52_2020"
+
+devices_run = [name_run_PC, name_run_PI, name_run_NANO, name_run_CORAL]
+devices_train = [name_train_PC, name_train_PI, name_train_NANO, name_train_CORAL]
+
+temp, temp1, temp2, temp3 = [], [], [], []
+
+data_PC_CPU, data_PI_CPU, data_NANO_CPU, data_CORAL_CPU = [], [], [], []
+data_PC_time, data_PI_time, data_NANO_time, data_CORAL_time = [], [], [], []
+
+data_train_CPU = [data_PC_CPU, data_PI_CPU, data_NANO_CPU, data_CORAL_CPU]
+data_train_time = [data_PC_time, data_PI_time, data_NANO_time, data_CORAL_time]
+
+cpu_percent = []
+virtual_mem = []
+time_diff = []
+
+programs = ["compair", "friction", "narendra4", "pt2",
+            "P0Y0_narendra4", "P0Y0_compair", "gradient"]
+labels_cpu = ["compair", "friction", "narendra4", "pt2",
+              "P0Y0_narendra4", "P0Y0_compair", "gradient", "no operations"]
+labels_time = ["compair", "friction", "narendra4", "pt2",
+               "P0Y0_narendra4", "P0Y0_compair", "gradient", "Totaal"]
+
+program_i = 0
+device_i = 0
+width = 0.22  # the width of the bars
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+for device in devices_train:
+    with open('./logging/' + device + ".csv", mode='r') as results_file:
+        results_reader = csv.DictReader(results_file)
+        for row in results_reader:
+            temp1.append(float(row['CPU Percentage']))
+            temp2.append(round(float(row['timediff']), 5))
+
+    for program in programs:
+        cpu_avg = round(mean(temp1[program_i * iterations:(program_i * iterations + iterations)]), 5)
+        time_diff_avg = round(mean(temp2[program_i * iterations:(program_i * iterations + iterations)]), 5)
+        program_i += 1
+
+        data_train_CPU[device_i].append(cpu_avg)
+        data_train_time[device_i].append(time_diff_avg)
+    data_train_CPU[device_i].append(temp1[-1])
+    data_train_time[device_i].append(temp2[-1])
+
+    program_i = 0
+    device_i += 1
+    temp1, temp2 = [], []
+
+x = np.arange(len(labels_time))  # the label locations
+
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - 3 * width / 2, data_train_time[0], width, label='PC')
+rects2 = ax.bar(x - width / 2, data_train_time[1], width, label='PI')
+rects3 = ax.bar(x + width / 2, data_train_time[2], width, label='NANO')
+rects4 = ax.bar(x + 3 * width / 2, data_train_time[3], width, label='CORAL')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Time program execution during training')
+ax.set_title('Time program execution for each device during training')
+ax.set_xticks(x)
+ax.set_xticklabels(labels_time)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+autolabel(rects4)
+
+fig.tight_layout()
+mng = plt.get_current_fig_manager()
+mng.window.state('zoomed')
+plt.yscale("log")
+plt.show()
+
+# plot cpu usage
+x = np.arange(len(labels_cpu))  # the label locations
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - 3 * width / 2, data_train_CPU[0], width, label='PC')
+rects2 = ax.bar(x - width / 2, data_train_CPU[1], width, label='PI')
+rects3 = ax.bar(x + width / 2, data_train_CPU[2], width, label='NANO')
+rects4 = ax.bar(x + 3 * width / 2, data_train_CPU[3], width, label='CORAL')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('CPU usage during training')
+ax.set_title('CPU usage for each device during training')
+ax.set_xticks(x)
+ax.set_xticklabels(labels_time)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+autolabel(rects4)
+
+fig.tight_layout()
+mng = plt.get_current_fig_manager()
+mng.window.state('zoomed')
+plt.show()
+
+# Plot running parameters
+data_PC_CPU, data_PI_CPU, data_NANO_CPU, data_CORAL_CPU = [], [], [], []
+data_PC_time, data_PI_time, data_NANO_time, data_CORAL_time = [], [], [], []
+
+data_run_CPU = [data_PC_CPU, data_PI_CPU, data_NANO_CPU, data_CORAL_CPU]
+data_run_time = [data_PC_time, data_PI_time, data_NANO_time, data_CORAL_time]
+
+program_i = 0
+device_i = 0
+
+for device in devices_run:
+    with open('./logging/' + device + ".csv", mode='r') as results_file:
+        results_reader = csv.DictReader(results_file)
+        for row in results_reader:
+            temp1.append(float(row['CPU Percentage']))
+            temp2.append(round(float(row['timediff']), 5))
+
+    for program in programs:
+        cpu_avg = round(mean(temp1[program_i * iterations:(program_i * iterations + iterations)]), 5)
+        time_diff_avg = round(mean(temp2[program_i * iterations:(program_i * iterations + iterations)]), 5)
+        program_i += 1
+
+        data_run_CPU[device_i].append(cpu_avg)
+        data_run_time[device_i].append(time_diff_avg)
+    data_run_CPU[device_i].append(temp1[-1])
+    data_run_time[device_i].append(temp2[-1])
+
+    program_i = 0
+    device_i += 1
+    temp1, temp2 = [], []
+
+x = np.arange(len(labels_time))  # the label locations
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - 3 * width / 2, data_run_time[0], width, label='PC')
+rects2 = ax.bar(x - width / 2, data_run_time[1], width, label='PI')
+rects3 = ax.bar(x + width / 2, data_run_time[2], width, label='NANO')
+rects4 = ax.bar(x + 3 * width / 2, data_run_time[3], width, label='CORAL')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Time program execution during running')
+ax.set_title('Time program execution for each device during running')
+ax.set_xticks(x)
+ax.set_xticklabels(labels_time)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+autolabel(rects4)
+
+fig.tight_layout()
+mng = plt.get_current_fig_manager()
+mng.window.state('zoomed')
+plt.yscale("log")
+plt.show()
+
+# plot cpu usage
+x = np.arange(len(labels_cpu))  # the label locations
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - 3 * width / 2, data_run_CPU[0], width, label='PC')
+rects2 = ax.bar(x - width / 2, data_run_CPU[1], width, label='PI')
+rects3 = ax.bar(x + width / 2, data_run_CPU[2], width, label='NANO')
+rects4 = ax.bar(x + 3 * width / 2, data_run_CPU[3], width, label='CORAL')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('CPU usage during running')
+ax.set_title('CPU usage for each device during running')
+ax.set_xticks(x)
+ax.set_xticklabels(labels_time)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+autolabel(rects4)
+
+fig.tight_layout()
+mng = plt.get_current_fig_manager()
+mng.window.state('zoomed')
+plt.show()
---
+++ Python/pyrenn/runV1.py
@@ -0,0 +1,263 @@
+import numpy as np
+from numpy import genfromtxt
+import pyrenn as prn
+import csv
+import time
+from statistics import mean
+import psutil
+
+cores = []
+cpu_percent = []
+virtual_mem = []
+time_start = []
+time_stop = []
+time_diff = []
+time_total_start = []
+time_total_end = []
+time_total = 0
+iterations = 20
+labels = ["compair", "friction", "narendra4", "pt2",
+          "P0Y0_narendra4", "P0Y0_compair", "gradient", "Totaal"]
+
+###
+# Creating a filename
+
+seconds = time.time()
+local_time = time.ctime(seconds)
+naam2 = local_time.split()
+naam = "MP_NN_ALL_RUN_PC"
+for i in range(len(naam2)):
+    naam += "_" + naam2[i]
+naam = naam.replace(':', '_')
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# first time calling cpu percent to get rid of 0,0
+psutil.cpu_percent(interval=None, percpu=True)
+time_total_start = time.time()
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example_compair.py
+for i in range(iterations):
+
+
+    # Read Example Data
+    df = genfromtxt('example_data_compressed_air.csv', delimiter=',')
+
+    P = np.array([df[1], df[2], df[3]])
+    Y = np.array([df[4], df[5]])
+    Ptest = np.array([df[6], df[7], df[8]])
+    Ytest = np.array([df[9], df[10]])
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/compair.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Calculate outputs of the trained NN for train and test data
+    y = prn.NNOut(P, net)
+    ytest = prn.NNOut(Ptest, net)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example_friction.py
+for i in range(iterations):
+    # Read Example Data
+    df = genfromtxt('example_data_friction.csv', delimiter=',')
+    P = df[1]
+    Y = df[2]
+    Ptest = df[3]
+    Ytest = df[4]
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/friction.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Calculate outputs of the trained NN for train and test data
+    y = prn.NNOut(P, net)
+    ytest = prn.NNOut(Ptest, net)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example_narendra4.py
+for i in range(iterations):
+    # Read Example Data
+    df = genfromtxt('example_data_narendra4.csv', delimiter=',')
+    P = df[1]
+    Y = df[2]
+    Ptest = df[3]
+    Ytest = df[4]
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/narendra4.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Calculate outputs of the trained NN for train and test data
+    y = prn.NNOut(P, net)
+    ytest = prn.NNOut(Ptest, net)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example_pt2.py
+for i in range(iterations):
+    # Read Example Data
+    df = genfromtxt('example_data_friction.csv', delimiter=',')
+    P = df[1]
+    Y = df[2]
+    Ptest = df[3]
+    Ytest = df[4]
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/pt2.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Calculate outputs of the trained NN for train and test data
+    y = prn.NNOut(P, net)
+    ytest = prn.NNOut(Ptest, net)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example_using_P0Y0_narendra4.py
+for i in range(iterations):
+    # Read Example Data
+    df = genfromtxt('example_data_narendra4.csv', delimiter=',')
+    P = df[1]
+    Y = df[2]
+    Ptest_ = df[3]
+    Ytest_ = df[4]
+
+    # define the first 3 timesteps t=[0,1,2] of Test Data as previous (known) data P0test and Y0test
+    P0test = Ptest_[0:3]
+    Y0test = Ytest_[0:3]
+    # Use the timesteps t = [3..99] as Test Data
+    Ptest = Ptest_[3:100]
+    Ytest = Ytest_[3:100]
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/using_P0Y0_narendra4.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Calculate outputs of the trained NN for test data with and without previous input P0 and output Y0
+    ytest = prn.NNOut(Ptest, net)
+    y0test = prn.NNOut(Ptest, net, P0=P0test, Y0=Y0test)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example__using_P0Y0_compair.py
+for i in range(iterations):
+    # Read Example Data
+    df = genfromtxt('example_data_compressed_air.csv', delimiter=',')
+
+    P = np.array([df[1], df[2], df[3]])
+    Y = np.array([df[4], df[5]])
+    Ptest_ = np.array([df[6], df[7], df[8]])
+    Ytest_ = np.array([df[9], df[10]])
+
+    # define the first timestep t=0 of Test Data as previous (known) data P0test and Y0test
+    P0test = Ptest_[:, 0:1]
+    Y0test = Ytest_[:, 0:1]
+    # Use the timesteps t = [1..99] as Test Data
+    Ptest = Ptest_[:, 1:100]
+    Ytest = Ytest_[:, 1:100]
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/using_P0Y0_compair.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Calculate outputs of the trained NN for test data with and without previous input P0 and output Y0
+    ytest = prn.NNOut(Ptest, net)
+    y0test = prn.NNOut(Ptest, net, P0=P0test, Y0=Y0test)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# example_gradient.py
+for i in range(iterations):
+    time_start.append(time.time())
+
+    df = genfromtxt('example_data_pt2.csv', delimiter=',')
+
+    P = df[1]
+    Y = df[2]
+
+    # Load saved NN from file
+    net = prn.loadNN("./SavedNN/gradient.csv")
+
+    psutil.cpu_percent(interval=None, percpu=True)
+    time_start.append(time.time())
+
+    # Prepare input Data for gradient calculation
+    data, net = prn.prepare_data(P, Y, net)
+
+    # Real Time Recurrent Learning
+    J, E, e = prn.RTRL(net, data)
+    g_rtrl = 2 * np.dot(J.transpose(), e)  # calculate g from Jacobian and error vector
+
+    # Back Propagation Through Time
+    g_bptt, E = prn.BPTT(net, data)
+
+    # Compare
+    # print('\n\n\nComparing Methods:')
+    # print('Time RTRL: ', (t1_rtrl - t0_rtrl), 's')
+    # print('Time BPTT: ', (t1_bptt - t0_bptt), 's')
+    # if not np.any(np.abs(g_rtrl - g_bptt) > 1e-9):
+    #    print('\nBoth methods showing the same result!')
+    #    print('g_rtrl/g_bptt = ', g_rtrl / g_bptt)
+
+    time_stop.append(time.time())
+    cores.append(psutil.cpu_percent(interval=None, percpu=True))
+    virtual_mem.append(psutil.virtual_memory())
+
+time_total_end = time.time()
+cores.append(psutil.cpu_percent(interval=None, percpu=True))
+virtual_mem.append(psutil.virtual_memory())
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Logging data
+for i in range(iterations*7):
+    time_diff.append(time_stop[i] - time_start[i])
+    time_total += time_stop[i] - time_start[i]
+time_diff.append(time_total/iterations)
+i = 0
+for core in cores:
+    cpu_percent.append(mean(cores[i]))
+    i += 1
+i = 0
+
+with open('./logging/' + naam + ".csv", mode='w') as results_file:
+    fieldnames = ['Naam', 'CPU Percentage', 'timediff', 'virtual mem']
+    file_writer = csv.DictWriter(results_file, fieldnames=fieldnames)
+    file_writer.writeheader()
+    for i in range(iterations*7+1):
+        j = int(i/iterations)
+        file_writer.writerow({'Naam': labels[j], 'CPU Percentage':  str(cpu_percent[i]), 'timediff': str(time_diff[i]),
+                              'virtual mem': str(virtual_mem[i])})
+
